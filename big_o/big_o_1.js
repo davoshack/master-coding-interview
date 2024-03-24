73282d40ba44947a6176185f57e491a3b2d8f1b3
Juan D Hernandez G <juandavos941@gmail.com>
2024-03-24 12:37:45 -0500

    O(1) -- Constant Time

--- big_o/big_o_1.js
+++ big_o/big_o_1.js
@@ -11,3 +11,10 @@ function findNemo(array) {
   console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
 }
 findNemo(large) // O(n) --> Linear Time
+
+// O(1) --> Constant Time
+const boxes = [0,1,2,3,4,5];
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]); // O(1)
+  console.log(boxes[1]); // O(1)
+}
