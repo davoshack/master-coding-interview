874370d69d424d992c2fc259f24cec3b8f7aa629
Juan D Hernandez G <juandavos941@gmail.com>
2024-03-25 11:04:42 -0500

    Big O rule 3 - Different inputs

---
+++ big_o/bit_o_rule_3.js
@@ -0,0 +1,11 @@
+function compressBoxesTwice(boxes) {
+  boxes.forEach(function(boxes) {
+    console.log(boxes);
+  });
+
+  boxes.forEach(function(boxes) {
+    console.log(boxes);
+  });
+}
+// Two differents inputs, but the same function
+// O(a + b)
